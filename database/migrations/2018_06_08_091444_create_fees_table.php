84026199feaf719a1931fe866578ea65f6a90a23
Brian Mutinda <mutindabrian26@gmail.com>
2018-06-13 19:20:17 +0300

    Initial Commit

---
+++ database/migrations/2018_06_08_091444_create_fees_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateFeesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('fees', function (Blueprint $table) {
+            // paying fees, date of payment and amount
+            $table->increments('transaction_id');
+            $table->integer('student_id'); //foriegn key to students table
+            $table->date('date_of_payment');
+            $table->integer('amount_paid');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('fees');
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,43 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('brian_mutinda/index');
+});
+
+/*Go to the fees page*/
+Route::get('/fees',function (){
+   return view ('brian_mutinda/fees');
+});
+
+/*Go to the student page*/
+Route::get('/student',function (){
+    return view('brian_mutinda/student');
+});
+
+/*Save student form*/
+Route::post('/student/save','studentController@store');
+
+/*Save fees information*/
+Route::post('/fees/save','feesController@store');
+
+/*See all students with their info*/
+Route::get('/all_students','studentController@show');
+
+/*See a student with their payment info*/
+Route::get('/student/student_detail/{id}','studentController@show_student_info');
+
+/*Search for a student*/
+Route::get('/student/search_student','studentController@search_student');
+
+// Route::resource('model', 'controller');
